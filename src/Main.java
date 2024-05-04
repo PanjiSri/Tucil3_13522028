23ef74bf3647b67829063bb795ae064dc2702992
PanjiSri <neiabaraja04@gmail.com>
2024-05-04 19:54:51 +0700

    fix: menggunakan dictionary.txt dari qna

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static final String WORDS_FILE_PATH = "words_alpha.txt";
+    private static final String WORDS_FILE_PATH = "dictionary.txt";
 
     public static void main(String[] args) {
 
